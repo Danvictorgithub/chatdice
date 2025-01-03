0ac6d707389348b127c55af37741a8486ede3b56
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-04 04:55:22 +0800

    modified header

--- frontend/src/components/Header2.tsx
+++ frontend/src/components/Header2.tsx
@@ -1,6 +1,6 @@
 import { Icon } from "@iconify-icon/solid";
 import { A } from "@solidjs/router";
-export default function Header() {
+export default function Header2() {
   return (
     <header class="border">
       <nav class="flex h-16 justify-between items-center px-4">
@@ -16,16 +16,6 @@ export default function Header() {
         </div>
         <div class="hidden sm:flex">
           <p class="text-gray-500 font-medium">Chat people near you</p>
-        </div>
-        <div class="flex basis-0 grow justify-end">
-          <div>
-            <A href="/auth/signin">
-              <button class="flex items-center gap-2">
-                <Icon icon="material-symbols:login" width="24" height="24" />
-                <p>sign in</p>
-              </button>
-            </A>
-          </div>
         </div>
       </nav>
     </header>
